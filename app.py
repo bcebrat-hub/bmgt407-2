ffb0ca960f3c214baf145a877cb6ecd6937b32a7
bencebrat <bcebrat@terpmail.umd.edu>
2026-02-06 11:13:53 -0500

    Added code to demonstrate first code change.

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World from Robert Smith! I am adding my first code change.'
-
 
 if __name__ == '__main__':
     app.run()
